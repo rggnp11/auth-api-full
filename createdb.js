5f4e366b3f9e02cc59c904a14d23328ce7fb025b
ctfrmht <contactform.ht@gmail.com>
2023-12-14 21:59:02 +0700

    Update createdb.js

--- createdb.js
+++ createdb.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
     database: 'postgres'
 });
 
